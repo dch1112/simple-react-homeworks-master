ebc1269e5b24c73996fc6c30b9e733886a31082e
Dmitriy678 <ched678@mail.ru>
2021-01-07 21:30:08 +0300

    Updates

---
+++ precache-manifest.765469093353296c04cda6b05bfa3447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e851275d385ee52535d1b84fd654c48",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "0ef57b8588bb605303dd",
+    "url": "/simple-react-homeworks-master/static/css/main.64e65668.chunk.css"
+  },
+  {
+    "revision": "dcf71578cd494d3427ab",
+    "url": "/simple-react-homeworks-master/static/js/2.050ec96a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks-master/static/js/2.050ec96a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef57b8588bb605303dd",
+    "url": "/simple-react-homeworks-master/static/js/main.b8f038e5.chunk.js"
+  },
+  {
+    "revision": "33f020c42a00208bc7a5",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.074d676d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-homeworks-master/precache-manifest.4d47b0bb489d5fe38c4b0d63395f50f3.js"
+  "/simple-react-homeworks-master/precache-manifest.765469093353296c04cda6b05bfa3447.js"
 );
 
 self.addEventListener('message', (event) => {

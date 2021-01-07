12bfdc149fe80f5a113cd8b664f28d1de4f5a2d2
Dmitriy678 <ched678@mail.ru>
2021-01-07 22:27:56 +0300

    Updates

--- precache-manifest.28f67bc7ea3ba682bf9e14a0034ada35.js
+++ precache-manifest.28f67bc7ea3ba682bf9e14a0034ada35.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "892f0a3d07407eb5abc1c8c5bab94be6",
+    "revision": "efec4fe17fd6e04de0f1019351efcdbf",
     "url": "/simple-react-homeworks-master/index.html"
   },
   {
-    "revision": "7e2b3b2b01c6c19dc7c2",
+    "revision": "2c6e1210d8619fc7581a",
     "url": "/simple-react-homeworks-master/static/css/main.64e65668.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/simple-react-homeworks-master/static/js/2.e350b370.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7e2b3b2b01c6c19dc7c2",
-    "url": "/simple-react-homeworks-master/static/js/main.326bae92.chunk.js"
+    "revision": "2c6e1210d8619fc7581a",
+    "url": "/simple-react-homeworks-master/static/js/main.3086c0c8.chunk.js"
   },
   {
     "revision": "33f020c42a00208bc7a5",
